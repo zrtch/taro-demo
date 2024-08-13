9b98ba5a748f14cf8c52cb9e857d6c82f2e610e7
zrt <15970334351@163.com>
2024-08-13 10:55:54 +0800

    update：webpack打包时间

--- config/index.ts
+++ config/index.ts
@@ -1,104 +1,126 @@
-import { defineConfig, type UserConfigExport } from '@tarojs/cli'
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
-import devConfig from './dev'
-import prodConfig from './prod'
+import { defineConfig, type UserConfigExport } from "@tarojs/cli";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import devConfig from "./dev";
+import prodConfig from "./prod";
+import Os from "os";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 // https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
 export default defineConfig(async (merge, { command, mode }) => {
   const baseConfig: UserConfigExport = {
-    projectName: 'taro-demo',
-    date: '2024-4-24',
+    projectName: "taro-demo",
+    date: "2024-4-24",
     designWidth: 750,
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
       375: 2,
-      828: 1.81 / 2
+      828: 1.81 / 2,
     },
-    sourceRoot: 'src',
-    outputRoot: 'dist',
+    sourceRoot: "src",
+    outputRoot: "dist",
     plugins: [],
-    defineConstants: {
-    },
+    defineConstants: {},
     copy: {
-      patterns: [
-      ],
-      options: {
-      }
+      patterns: [],
+      options: {},
     },
-    framework: 'react',
-    compiler: 'webpack5',
+    // 配置include/exclude缩小Loader对文件的搜索范围
+    rules: [
+      {
+        exclude: /node_modules/,
+        include: /src/,
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: { cacheDirectory: true },
+          },
+          // 并行构建
+          {
+            loader: "thread-loader",
+            options: { workers: Os.cpus().length },
+          },
+        ],
+      },
+    ],
+    framework: "react",
+    compiler: "webpack5",
     cache: {
-      enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    },
+    prebundle: {
+      enable: true,
     },
     mini: {
       postcss: {
         pxtransform: {
           enable: true,
-          config: {
-
-          }
+          config: {},
         },
         url: {
           enable: true,
           config: {
-            limit: 1024 // 设定转换尺寸上限
-          }
+            limit: 1024, // 设定转换尺寸上限
+          },
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
+            namingPattern: "module", // 转换模式，取值为 global/module
+            generateScopedName: "[name]__[local]___[hash:base64:5]",
+          },
+        },
       },
       webpackChain(chain) {
-        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
-      }
+        chain.resolve.plugin("tsconfig-paths").use(TsconfigPathsPlugin);
+        chain
+          .plugin("analyzer")
+          .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin, []);
+      },
     },
     h5: {
-      publicPath: '/',
-      staticDirectory: 'static',
+      publicPath: "/",
+      staticDirectory: "static",
       output: {
-        filename: 'js/[name].[hash:8].js',
-        chunkFilename: 'js/[name].[chunkhash:8].js'
+        filename: "js/[name].[hash:8].js",
+        chunkFilename: "js/[name].[chunkhash:8].js",
       },
       miniCssExtractPluginOption: {
         ignoreOrder: true,
-        filename: 'css/[name].[hash].css',
-        chunkFilename: 'css/[name].[chunkhash].css'
+        filename: "css/[name].[hash].css",
+        chunkFilename: "css/[name].[chunkhash].css",
       },
       postcss: {
         autoprefixer: {
           enable: true,
-          config: {}
+          config: {},
         },
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
           config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]'
-          }
-        }
+            namingPattern: "module", // 转换模式，取值为 global/module
+            generateScopedName: "[name]__[local]___[hash:base64:5]",
+          },
+        },
       },
       webpackChain(chain) {
-        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
-      }
+        chain.resolve.plugin("tsconfig-paths").use(TsconfigPathsPlugin);
+      },
     },
     rn: {
-      appName: 'taroDemo',
+      appName: "taroDemo",
       postcss: {
         cssModules: {
           enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        }
-      }
-    }
-  }
-  if (process.env.NODE_ENV === 'development') {
+        },
+      },
+    },
+  };
+  if (process.env.NODE_ENV === "development") {
     // 本地开发构建配置（不混淆压缩）
-    return merge({}, baseConfig, devConfig)
+    return merge({}, baseConfig, devConfig);
   }
   // 生产构建配置（默认开启压缩混淆等）
-  return merge({}, baseConfig, prodConfig)
-})
+  return merge({}, baseConfig, prodConfig);
+});
